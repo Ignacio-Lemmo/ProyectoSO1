e4a65966a033aac97c36933ce81fa02b45a45245
Ignacio Lemmo <ilemmo@correo.unimet.edu.ve>
2022-05-13 12:35:04 -0400

    Primer commit

---
+++ src/proyectoso1/ProyectoSO1.java
@@ -0,0 +1,13 @@
+package proyectoso1;
+
+/**
+ *
+ * @author Ignacio Lemmo y Maria López
+ */
+public class ProyectoSO1 {
+
+    public static void main(String[] args) {
+        System.out.println("Inicio del Proyecto");
+    }
+    
+}
